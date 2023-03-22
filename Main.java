e7b5f6a6c53aea3cba8de7b540eb121444e00e82
WhiteMirac1e <115178943+WhiteMirac1e@users.noreply.github.com>
2023-03-22 10:40:10 +0300

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+        CalculatorView view = new ConsoleCalculatorView();
+        CalculatorPresenter presenter = new CalculatorPresenterImpl(view);
+        view.setPresenter(presenter);
+        view.start();
+    }
+}
